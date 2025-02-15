575062f7e26328b0329603bb013e8c6cc44cf13c
paulo <kobeuko@gmail.com>
2025-02-15 17:59:00 -0600

    added files

--- src/main/java/com/mljsphack/mljsphack/MljspApplication.java
+++ src/main/java/com/mljsphack/mljsphack/MljspApplication.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
 public class MljsphackApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MljsphackApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MljsphackApplication.class, args);
+    }
 
 }
