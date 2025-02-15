822827ca9edef98557545f57be25e9160e13beee
paulo <kobeuko@gmail.com>
2025-02-15 17:47:51 -0600

    file

---
+++ src/main/java/com/mljsphack/mljsphack/MljsphackApplication.java
@@ -0,0 +1,13 @@
+package main.java.com.mljsphack.mljsphack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MljsphackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MljsphackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mljsphack/mljsphack/MljsphackApplicationTests.java
@@ -0,0 +1,13 @@
+package test.java.com.mljsphack.mljsphack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MljsphackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
